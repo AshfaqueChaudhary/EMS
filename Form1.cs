9e24e0b2deca00c0f8054e7fc49d7963d7faafb1
Ashfaque <Ashfaque@ASHFAQUE>
2020-02-17 15:57:50 +0300

    added my additionlib01 dll

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyAdditionLib01;
 
 namespace EMS
 {
@@ -40,6 +41,13 @@ namespace EMS
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hi");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyAdditionLib myAdditionLib = new MyAdditionLib();
+            int result = myAdditionLib.Add(10, 15);
+            MessageBox.Show(result.ToString());
         }
     }
 }
